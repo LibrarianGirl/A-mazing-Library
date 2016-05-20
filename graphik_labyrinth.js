3517e2dc9a782973201973e9d21e2012f7ee95b5
cm093 <c_mars@outlook.de>
2016-05-20 11:30:18 +0200

    Sprite + Kollisionserkennung
    Jetzt gibts schon mal ne erste Spielfigur

--- graphik_labyrinth.js
+++ graphik_labyrinth.js
@@ -7,11 +7,11 @@ function Spiel() {
     context = canvas.getContext("2d"); //ermöglicht das Zeichnen von 4ecken
     var w = canvas.width;
     var h = canvas.height; 
-//d:delimiter
-var delimiter = 15;
-var size = w/delimiter;
+	//d:delimiter
+	var delimiter = 15;
+	var size = w/delimiter;
 
- var Maze = new Array(
+	var Maze = new Array(
 							//  1,2,3,4,5,6,7,8,9,A,B,C,D,E,F
                       new Array(0,0,1,1,1,1,1,1,1,1,1,1,1,1,1),//1
                       new Array(0,0,1,0,0,0,0,0,0,0,0,1,1,1,1),//2
@@ -30,18 +30,40 @@ var size = w/delimiter;
 					  new Array(1,1,1,1,1,1,1,1,1,1,1,1,0,0,2)//F
                       ); 
 
-function BlackOrWhite(something){
-    if(something == 1){return "black";}
-    if(something == 2){return "red";}
-    else{return "white";};
-};
+	function BlackOrWhite(something){
+		if(something == 1){return "black";}
+		if(something == 2){return "red";}
+		else{return "white";};
+	};
 
-for(var i = 0;i<15;i++){
+	for(var i = 0;i<15;i++){
         for(var j = 0;j<15;j++){       
             rect(size*j,size*i,size,size,BlackOrWhite((Maze[i][j])));
 
     };
 };
+
+
+//function Spielen() {
+	var player = {
+    x: 0,
+    y: 0
+	};
+
+	//Draw the player
+	context.beginPath();
+	var half = size/2;
+	context.fillStyle = "blue";
+    context.arc(player.x*size+half, player.y*size+half, half, 0, 2*Math.PI);
+    context.fill();	
+	
+//};	
+
+
+//Check to see if the new space is inside the board and not a wall
+function canMove(x, y){
+    return (y>=1) && (y<w) && (x >= 1) && (x < h) && (board[y][x] != 0);
+}
 
 //+++++++++++++++++++++++++++++++++++++++++
 //     CANVVAS UTILITIEZ
