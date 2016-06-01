512bf01e7b3d8764f49aee6b9ed923189035c45f
LibrarianGirl <AnnaWe95@t-online.de>
2016-06-01 11:43:46 +0200

    Wand erkennung
    erkennt die Wand

--- graphik_labyrinth.js
+++ graphik_labyrinth.js
@@ -1,31 +1,5 @@
 function Game() {
-	var canvas, 
-		context,
-		bounce = -1;
-			
-		canvas = document.getElementById("stage")
-		context = canvas.getContext("2d"); //ermöglicht das Zeichnen von 4ecken
-		var w = canvas.width;
-		var h = canvas.height; 
-		//d:delimiter
-		var delimiter = 15;
-		var size = w/delimiter;
-		var player = {
-			x: 0,
-			y: 0
-		};
-	
-	Maze()
-	Player()
-	move()
-
-	//+++++++++++++++++++++++++++++++++++++++++
-	//     	UTILITIEZ
-	//+++++++++++++++++++++++++++++++++++++++++
-
-//Labyrinth
-	function Maze() { 
-			var Maze = new Array(
+	var MyMaze = new Array(
 								//  1,2,3,4,5,6,7,8,9,A,B,C,D,E,F
 						  new Array(0,0,1,1,1,1,1,1,1,1,1,1,1,1,1),//1
 						  new Array(0,0,1,0,0,0,0,0,0,0,0,1,1,1,1),//2
@@ -42,7 +16,34 @@ function Game() {
 						  new Array(1,1,1,1,1,1,1,1,0,1,0,0,0,0,1),//D
 						  new Array(1,1,1,1,1,1,1,1,0,0,0,0,0,1,1),//E
 						  new Array(1,1,1,1,1,1,1,1,1,1,1,1,0,0,2)//F
-						  ); 
+						  ); ;
+	var canvas, 
+		context,
+		bounce = -1;
+			
+		canvas = document.getElementById("stage")
+		context = canvas.getContext("2d"); //ermöglicht das Zeichnen von 4ecken
+		var w = canvas.width;
+		var h = canvas.height; 
+		//d:delimiter
+		var delimiter = 15;
+		var size = w/delimiter;
+		var player = {
+			x: 0,
+			y: 0
+		};
+	
+	Maze();
+	Player();
+	move();
+
+	//+++++++++++++++++++++++++++++++++++++++++
+	//     	UTILITIEZ
+	//+++++++++++++++++++++++++++++++++++++++++
+
+//Labyrinth
+	function Maze() { 
+			
 
 		function BlackOrWhite(something){
 			if(something == 1){return "black";}
@@ -52,7 +53,7 @@ function Game() {
 
 		for(var i = 0;i<15;i++){
 			for(var j = 0;j<15;j++){       
-				rect(size*j,size*i,size,size,BlackOrWhite((Maze[i][j])));
+				rect(size*j,size*i,size,size,BlackOrWhite((MyMaze[i][j])));
 
 			};
 		};
@@ -65,10 +66,9 @@ function Game() {
 	
 //Spieler	
 	function Player() { //setzt Player an den Anfang
-		
-		
-		
+	console.log(player.x+" "+player.y)
 		//Draw the player
+		
 		context.beginPath();
 		var half = size/2;
 		context.fillStyle = "blue";
@@ -80,33 +80,32 @@ function Game() {
 	function move(e){
 		
 		$(document).keyup(function(e){
-			if(e.which == 38) //&& canMove(player.x, player.y-1))//Up arrow
+			if((e.which == 38) && canMove(player.x, player.y-1))//Up arrow
 				player.y--;
-			else if(e.which == 40) //&& canMove(player.x, player.y+1)) // down arrow
+			else if((e.which == 40) && canMove(player.x, player.y+1)) // down arrow
 				player.y++;
-			else if(e.which == 37) //&& canMove(player.x-1, player.y))
+			else if((e.which == 37) && canMove(player.x-1, player.y))
 				player.x--;
-			else if(e.which == 39) //&& canMove(player.x+1, player.y))
+			else if((e.which == 39) && canMove(player.x+1, player.y))
 				player.x++;
 			
-			Maze()
-			context.beginPath();
-			var half = size/2;
-			context.fillStyle = "blue";
-			context.arc(player.x*size+half, player.y*size+half, half, 0, 2*Math.PI);
-			context.fill();
+			Maze();
+			Player();
 			e.preventDefault();
 		});
 	};
 	
+	
 
-/*
 	//Check to see if the new space is inside the board and not a wall
-	function canMove(x, y){
-		return (player.y>=1) && (player.y<w) && (player.x >= 1) && (player.x < h) && (new Array[player.y][player.x] != 0);
-			
-			
-	};	*/
+	function canMove(x,y){
+		if (x<0){return false;}
+		else if (y<0){return false;}
+		else if (x>=delimiter){return false;}
+		else if (y>=delimiter){return false;}
+		else if (MyMaze[y][x] ==1){return false;}
+		else {return true;}	
+	};	
 
 };
 
