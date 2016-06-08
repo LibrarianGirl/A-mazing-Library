84b17e3cce264c4defa7228065fdf4fd327ccb06
LibrarianGirl <AnnaWe95@t-online.de>
2016-06-08 13:09:10 +0200

    Bücherwagen
    Der Bücherwagen bewegt sich und wird erst im 2.  Level korrekt geladen.
    Der Sprite läuft nicht auf den Wagen.

--- graphik_labyrinth.js
+++ graphik_labyrinth.js
@@ -9,7 +9,7 @@ function Game() {
 	var MyMaze = new Array(												// Labyrinth Level 1
 								//  1,2,3,4,5,6,7,8,9,A,B,C,D,E,F
 						  new Array(0,0,1,1,1,1,1,1,1,1,1,1,1,1,1),//1
-						  new Array(0,0,1,0,0,0,0,0,0,0,0,1,1,1,1),//2
+						  new Array(2,0,1,0,0,0,0,0,0,0,0,1,1,1,1),//2
 						  new Array(1,0,1,0,1,1,1,0,0,0,0,0,0,1,1),//3
 						  new Array(1,0,1,0,0,0,1,0,0,1,1,1,0,1,1),//4
 						  new Array(1,0,1,1,1,0,1,1,1,1,1,1,0,0,1),//5
@@ -29,7 +29,7 @@ function Game() {
 		var MyMaze2 = new Array( 											//Labyrinth Level 2
 								//  1,2,3,4,5,6,7,8,9,A,B,C,D,E,F
 						  new Array(0,0,0,0,0,0,0,0,1,1,1,1,1,1,1),//1
-						  new Array(1,0,1,0,1,1,1,0,0,0,0,0,0,0,0),//2
+						  new Array(1,2,1,0,1,1,1,0,0,0,0,0,0,0,0),//2
 						  new Array(1,0,1,0,0,0,0,0,1,1,1,1,1,1,1),//3
 						  new Array(1,0,1,1,1,1,1,0,0,0,0,0,0,0,1),//4
 						  new Array(1,0,1,1,0,0,0,0,1,0,1,1,0,1,1),//5
@@ -38,7 +38,7 @@ function Game() {
 						  new Array(1,1,1,1,1,0,0,1,0,1,1,0,0,1,1),//8
 						  new Array(1,1,0,0,0,0,1,1,0,1,0,0,1,1,1),//9
 						  new Array(1,1,0,1,1,0,1,1,0,0,0,1,1,1,1),//A
-						  new Array(1,1,0,0,0,0,0,0,1,1,1,1,1,1,1),//B
+						  new Array(1,1,0,3,0,0,0,3,1,1,1,1,1,1,1),//B
 						  new Array(1,1,0,1,1,0,1,0,0,0,0,0,1,1,1),//C
 						  new Array(0,0,0,1,0,0,1,1,0,1,1,0,0,1,1),//D
 						  new Array(1,1,1,1,0,1,1,0,0,1,1,1,0,0,0),//E
@@ -49,7 +49,7 @@ function Game() {
 	Levels.push(MyMaze);
 	Levels.push(MyMaze2);
 //	Levels.push(MyMaze3);
-console.log(Levels);
+	//console.log(currentLevel);
 	
 //Definiert den Canvas
 	var canvas, 
@@ -61,7 +61,7 @@ console.log(Levels);
 		var w = canvas.width;
 		var h = canvas.height; 
 		//d:delimiter
-		var delimiter = 15;
+		var delimiter = 15;     // ermöglicht die Einteilung des Canvas in 15 Quadrate
 		var size = w/delimiter;
 		
 //Setzt den  Player auf Position 0	
@@ -72,16 +72,16 @@ console.log(Levels);
 		
 //setzt den Bücherwagen auf Position
 		var PosWagen = {
-			x: 3,
-			y: 11
+			x: 0,
+			y: 0
 		};
 		
 // Eigentlicher Spielablauf
 	Maze();
 	Player();
-	Bücherwagen();
 	move();
 	moveWagen();
+
 	
 
 	//+++++++++++++++++++++++++++++++++++++++++
@@ -107,8 +107,20 @@ console.log(Levels);
 		context.fillStyle = color;  
 		context.fillRect (x,y,w,h);  
 		};
-	};	
 		
+	
+		//Lädt  den Bücherwagen im 2. Level
+		if (currentLevel == 1) {Bücherwagen()};
+	};	
+
+	//Bücherwagen laden
+	function WagenLaden (){
+			if (currentLevel == 1) {PosWagen = {
+									x: 5,
+									y: 10
+									};
+		};
+	};	
 	
 //Zeichnet den Spieler	
 	function Player() { 		
@@ -143,43 +155,56 @@ console.log(Levels);
 			
 			Maze();
 			Player();
-			Bücherwagen();
 			e.preventDefault();
+			//console.log(player);
 		});
 	};
 	
-//Kollisions erkennung mit Wand und Rand und Ende des Levels
+//Kollisions erkennung mit Wand, Rand und Ende des Levels
 	function canMove(x,y){
-		if (x<0){return false;}
+		if (x<0){return false;} //erkennt den Rand
 		else if (y<0){return false;}
-		else if (x>=delimiter){return false;}
+		else if (x>=delimiter){return false;} 
 		else if (y>=delimiter){return false;}
-		else if (currentMaze[y][x] ==1){return false;}
-		else if (currentMaze[y][x] ==2){alert('Gewonnen!');currentLevel++; player.x=0; player.y=0;}
+		
+		else if (currentMaze[y][x] ==1){return false;} // erkennt die Wand
+		
+		else if ((x == PosWagen.x)&& (y==PosWagen.y)) {return false;} // erkennt den Wagen 
+		
+		else if (currentMaze[y][x] ==2){alert('Gewonnen!');currentLevel++; player.x=0; player.y=0;WagenLaden();} //erkennt das Tor
+		else {return true;}	
+		//console.log (PosWagen);
+		
+	};	
+
+//Bücherwagen bewegen 
+    function moveWagen(e){
+	
+		$(document).keyup(function(e){
+		
+				
+				if((e.which == 38 || e.which == 40 ) && canMoveWagen(PosWagen.x-1, PosWagen.y)) //Hoch und Runter: Links
+					PosWagen.x--;
+				if((e.which == 37 || e.which == 39 ) && canMoveWagen(PosWagen.x+1, PosWagen.y)) // Rechts und Links: Rechts
+					PosWagen.x++;
+			});
+		
+	
+		Maze();
+		Player();
+		
+	};
+	
+	function canMoveWagen (x,y){
+		if (currentMaze[y][x] ==3){return false;} // erkennt die Wand
+	
 		else {return true;}	
 	};	
-
-//Bücherwagen bewegen Entwurf
-
-    function moveWagen(e){
-		$(document).keyup(function(e){
-				if((e.which == 38) && canMove(player.x, player.y-1) && )//Hoch drücken -> nach links bewegen
-					PosWagen.x--;
-				else if((e.which == 40) && canMove(player.x, player.y+1)) //Runter drücken -> nach links bewegen
-					PosWagen.x--;
-				else if((e.which == 37) && canMove(player.x-1, player.y)) //Links drücken -> nach rechts bewegen
-					PosWagen.x++;
-				else if((e.which == 39) && canMove(player.x+1, player.y)) //Rechts drücken -> nach rechts bewegen
-					PosWagen.x++;	
-		});		
-	};	
 	
-	
-	for a = 0 
 	
 
 	
-};
+};// ende Game
 
 
 
